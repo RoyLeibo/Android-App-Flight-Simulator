c232edb987a04043614a4978427ca96a51274b1d
Roy Leibovitz <royleibo212@gmail.com>
2019-06-14 20:39:44 +0300

    Create connection to server

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    Socket socket;
+    DataOutputStream stream;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button connectButton = findViewById(R.id.connect);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                EditText ip = findViewById(R.id.EditText_ipAddress);
+                EditText port = findViewById(R.id.EditText_port);
+                try {
+                    socket = new Socket(ip.getText().toString(), Integer.parseInt(port.getText().toString()));
+                    stream = new DataOutputStream(socket.getOutputStream());
+                }
+                catch (IOException e){}
+            }
+        });
+        //joysticMoveFunc(){
+            // the value is missing
+        try {
+            String aileronCommand = "set /controls/flight/aileron " + "\r\n";
+            String elevatorCommand = "set /controls/flight/elevator " + "\r\n";
+            stream.writeUTF(aileronCommand);
+            stream.writeUTF(elevatorCommand);
+        }
+        catch (IOException e){}
+        //}
     }
 }
