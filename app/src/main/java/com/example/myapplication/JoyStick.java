e1beb92b50e00532d78ae35f0948e6a79181b75e
Roy Leibovitz <royleibo212@gmail.com>
2019-06-16 18:54:53 +0300

    create connection in other thread

--- app/src/main/java/com/example/myapplication/JoyStick.java
+++ app/src/main/java/com/example/myapplication/JoyStick.java
@@ -26,6 +26,7 @@ public class JoyStick extends AppCompatActivity {
     JoyStickClass js;
     Socket socket;
     DataOutputStream stream;
+    Thread connectionThread;
 
     @SuppressLint("ClickableViewAccessibility")
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,46 +36,49 @@ public class JoyStick extends AppCompatActivity {
         Intent intent = getIntent();
         String msg = intent.getStringExtra("message");
         int split = msg.indexOf('$');
-        String ip = msg.substring(0,split);
-        String port = msg.substring(split + 1);
-
-        try {
-            InetAddress serverAddr = InetAddress.getByName(ip);
-            this.socket = new Socket(serverAddr, Integer.parseInt(port));
-            this.stream = new DataOutputStream(socket.getOutputStream());
-        } catch (Exception e) {
-            Log.e("TCP", "C: Error", e);
-        }
-
-        layout_joystick = (RelativeLayout)findViewById(R.id.layout_joystick);
-
-        js = new JoyStickClass(getApplicationContext(), layout_joystick, R.drawable.image_button);
-        js.setStickSize(150, 150);
-        js.setLayoutSize(500, 500);
-        js.setLayoutAlpha(150);
-        js.setStickAlpha(100);
-        js.setOffset(90);
-        js.setMinimumDistance(50);
-
-        layout_joystick.setOnTouchListener(new View.OnTouchListener() {
-            public boolean onTouch(View arg0, MotionEvent arg1) {
-                js.drawStick(arg1);
-                if(arg1.getAction() == MotionEvent.ACTION_DOWN
-                        || arg1.getAction() == MotionEvent.ACTION_MOVE) {
-                    x = js.getX();
-                    y = js.getY();
-                    int direction = js.get8Direction();
-                } else if(arg1.getAction() == MotionEvent.ACTION_UP) {
-                    try {
-                        stream.writeUTF("set /controls/flight/aileron " + x + "\r\n");
-                        stream.writeUTF("set /controls/flight/elevator " + y + "\r\n");
-                    }
-                    catch (IOException e){}
+        final String ip = msg.substring(0,split);
+        final int port = Integer.parseInt(msg.substring(split + 1));
+        this.connectionThread = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    InetAddress serverAddr = InetAddress.getByName(ip);
+                    socket = new Socket(serverAddr, port);
+                    stream = new DataOutputStream(socket.getOutputStream());
+                } catch (Exception e) {
+                    Log.e("TCP", "C: Error", e);
                 }
-                return true;
+
+                layout_joystick = (RelativeLayout)findViewById(R.id.layout_joystick);
+
+                js = new JoyStickClass(getApplicationContext(), layout_joystick, R.drawable.image_button);
+                js.setStickSize(150, 150);
+                js.setLayoutSize(500, 500);
+                js.setLayoutAlpha(150);
+                js.setStickAlpha(100);
+                js.setOffset(90);
+                js.setMinimumDistance(50);
+                layout_joystick.setOnTouchListener(new View.OnTouchListener() {
+                    public boolean onTouch(View arg0, MotionEvent arg1) {
+                        js.drawStick(arg1);
+                        if (arg1.getAction() == MotionEvent.ACTION_DOWN
+                                || arg1.getAction() == MotionEvent.ACTION_MOVE) {
+                            x = js.getX();
+                            y = js.getY();
+                            int direction = js.get8Direction();
+                            try {
+                                stream.writeUTF("set /controls/flight/aileron " + x + "\r\n");
+                                stream.writeUTF("set /controls/flight/elevator " + y + "\r\n");
+                            }
+                            catch (IOException e){}
+                        }
+                        return true;
+                    }
+                });
             }
         });
+        this.connectionThread.start();
     }
+
     public void onDestroy() {
         super.onDestroy();
         try {
@@ -82,6 +86,6 @@ public class JoyStick extends AppCompatActivity {
         } catch (IOException e){
             Log.e("TCP", "C: Error closing the socket", e);
         }
+        this.connectionThread.interrupt();
     }
-
 }
